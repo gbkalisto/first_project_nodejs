ba0f4fdb9caf6394959e76ae92369d2159c38656
shubham sharma <shubhamskv5@gmail.com>
2026-04-14 14:36:12 +0530

    change validation rule for the category

--- routes/admin/categoryRoute.js
+++ routes/admin/categoryRoute.js
@@ -8,7 +8,7 @@ router.get('/categories', categoryController.index)
 router.get('/categories/create', categoryController.create)
 router.post('/categories', upload('categories').single('image'), categoryValidationRules, validate, categoryController.store)
 router.get('/categories/:id/edit', categoryController.edit)
-router.put('/categories/:id/edit', upload('categories').single('image'), categoryValidationRules, validate, categoryController.update)
+router.put('/categories/:id/edit', upload('categories').single('image'), categoryController.update)
 router.delete('/categories/delete/:id', categoryController.delete);
 
 
